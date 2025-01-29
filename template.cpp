551320d8bc8d7ef9f98acadd03f275a6df24c83c
nsh07 <66912344+nsh07@users.noreply.github.com>
2025-01-29 08:46:08 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -3,8 +3,16 @@
 //
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+template <typename T1, typename T2>
+void inputVector(T1 n, vector<T2> &a) {
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+}
 
 int main() {
     ios::sync_with_stdio(0);
